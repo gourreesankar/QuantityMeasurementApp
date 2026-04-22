322dc606f93cae40fa292431ef10c4313cfdc150
gourreesankar <goureesankarroy@gmail.com>
2026-04-22 10:42:17 +0530

    UC3: Generic Length class - DRY Principle

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,27 +2,32 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-    public static class Feet {
-        private final double value;
+    public static boolean demonstrateLengthEquality(Length length1, Length length2) {
+        return length1.equals(length2);
+    }
 
-        public Feet(double value) {
-            this.value = value;
-        }
+    public static void demonstrateFeetEquality() {
+        Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length l2 = new Length(1.0, Length.LengthUnit.FEET);
+        System.out.println("Are 1.0 ft and 1.0 ft equal? " + demonstrateLengthEquality(l1, l2));
+    }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null) return false;
-            if (getClass() != obj.getClass()) return false;
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
+    public static void demonstrateInchesEquality() {
+        Length l1 = new Length(1.0, Length.LengthUnit.INCHES);
+        Length l2 = new Length(1.0, Length.LengthUnit.INCHES);
+        System.out.println("Are 1.0 inch and 1.0 inch equal? " + demonstrateLengthEquality(l1, l2));
+    }
+
+    public static void demonstrateFeetInchesComparison() {
+        Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length l2 = new Length(12.0, Length.LengthUnit.INCHES);
+        System.out.println("Are 1.0 ft and 12.0 inches equal? " + demonstrateLengthEquality(l1, l2));
     }
 
     public static void main(String[] args) {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
         System.out.println("=== Quantity Measurement App ===");
-        System.out.println("Are 1.0 ft and 1.0 ft equal? " + feet1.equals(feet2));
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
+        demonstrateFeetInchesComparison();
     }
 }
